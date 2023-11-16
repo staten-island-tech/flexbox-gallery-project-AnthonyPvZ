ea150a5188a495be5427768b84284415d6862409
AnthonyPvZ <anthonypvz123@gmail.com>
2023-11-16 09:20:47 -0500

    Update main.js

--- vite-project/main.js
+++ vite-project/main.js
@@ -10,16 +10,19 @@ meleebutton: document.querySelector("#Melee"),
 }
 dom.meleebutton.addEventListener("click", function (sortmelee){
   sortmelee.preventDefault();
-  dom.container.innerHTML = `<div class="card" id = "gun">
+  dom.container.innerHTML = `
+  <div class="card" id = "gun">
   <h2 class="card-title">Boomstick</h2>
   <img src="Boomstick.png" alt="" class="card-img">
   <p class="card-desc">This shotgun allows you to load up to four shells in the chamber at once, propelling
       you
-      backwards when fired. Hold down reload for faster loading of each shell.</p>         <div class="card">
-      <h2 class="card-title">'Hyperion' Heavy Laser</h2>
-      <img src="hyperion.png" alt="" class="card-img">
-      <p class="card-desc">Heavy laser cannon with very slow fire rate but massive burst damage. Lasers have a
-          change of piercing and have innate flame rounds. Loses damage over long distance.</p>
+      backwards when fired. Hold down reload for faster loading of each shell.</p>
+
+  <div class="card">
+  <h2 class="card-title">'Hyperion' Heavy Laser</h2>
+  <img src="hyperion.png" alt="" class="card-img">
+  <p class="card-desc">Heavy laser cannon with very slow fire rate but massive burst damage. Lasers have a
+      change of piercing and have innate flame rounds. Loses damage over long distance.</p>
 
   </div>`
 });

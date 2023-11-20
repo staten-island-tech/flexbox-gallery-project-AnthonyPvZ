e1e2bf5794653cb85b0a8d353370c01774cd2ced
AnthonyPvZ <anthonypvz123@gmail.com>
2023-11-20 01:09:34 -0500

    array

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,29 +1,40 @@
 import './style.css'
 
 // filter guns and melee function
-const dom = {
-container: document.querySelector(".container"),
-guns: document.querySelectorAll(".gun"),
-melee: document.querySelectorAll(".melee"),
-gunbutton: document.querySelector("#Guns"),
-meleebutton: document.querySelector("#Melee"),
-}
-dom.meleebutton.addEventListener("click", function (sortmelee){
-  sortmelee.preventDefault();
-  dom.container.innerHTML = `
-  <div class="card" id = "gun">
-  <h2 class="card-title">Boomstick</h2>
-  <img src="Boomstick.png" alt="" class="card-img">
-  <p class="card-desc">This shotgun allows you to load up to four shells in the chamber at once, propelling
-      you
-      backwards when fired. Hold down reload for faster loading of each shell.</p>
+const cards = [{
+  title: "Boomstick",
+  image: "/Boomstick.png",
+  description: "This shotgun allows you to load up to four shells in the chamber at once, propelling you backwards when fired. Hold down reload for faster loading of each shell.",
+},
+{
+  title: "'Hyperion' Heavy Laser",
+  image: "/hyperion.png",
+  description: "testHeavy laser cannon with very slow fire rate but massive burst damage. Lasers have a change of piercing and have innate flame rounds. Loses damage over long distance.",
+}];
 
-  <div class="card">
-  <h2 class="card-title">'Hyperion' Heavy Laser</h2>
-  <img src="hyperion.png" alt="" class="card-img">
-  <p class="card-desc">Heavy laser cannon with very slow fire rate but massive burst damage. Lasers have a
-      change of piercing and have innate flame rounds. Loses damage over long distance.</p>
+const container = document.querySelector("#container");
 
-  </div>`
-});
+cards.forEach((result, y) => {
+  const card = document.createElement('div');
+  card.classlist = 'card';
+  const description = `<div class="card"> 
+    <h2 class="card-title">${result.title}</h2>
+    <img src="${result.image}" alt="" class="card-img">
+    <p class="card-desc">${result.description}</p>
+  </div>`;
+  container.innerHTML += description;
+})
+
+  /* <div class="card">
+            <h2 class="card-title">Murasama</h2>
+            <img src="murasama.png" alt="" class="card-img">
+            <p class="card-desc">It's the only thing I know for real…
+                Can dodge in all directions.
+                Can perfect block/parry with good timing, greatly boosting attacks and dodges. Has a 3 hit combo.
+                Regular
+                blocking gives a slight boost.
+                Consumes meter on swing.</p>
+        </div> */
+
+
 // https://stackoverflow.com/questions/54706080/generating-dynamic-html-cards-from-a-javascript-array

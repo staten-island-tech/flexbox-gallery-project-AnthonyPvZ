da6c00bf6d0874014fafdb7e6fb914a57d67cd70
AnthonyPvZ <anthonypvz123@gmail.com>
2023-12-06 09:03:31 -0500

    proof of work

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -129,3 +129,72 @@ buttonse.forEach((btn) => btn.addEventListener("click", function () {
     document.body.classList.add("dark")
   }
 }))
+
+
+/* old code
+const container = document.querySelector("#container");
+cards.forEach((result) => {
+  const card = document.createElement('div');
+  card.classlist = 'card';
+  const description = `<div class="card"> 
+    <h2 class="card-title">${result.title}</h2>
+    <img src="${result.image}" alt="" class="card-img">
+    <p class="card-desc">${result.description}</p>
+  </div>`;
+  container.innerHTML += description;
+})
+const buttons = {
+  Guns: document.querySelector("#Guns"),
+  Melees: document.querySelector("#Melee"),
+  Contraband: document.querySelector("#Contraband")
+}
+buttons.Guns.addEventListener("click", function (filtergun) {
+  filtergun.preventDefault();
+  container.innerHTML = ""
+  cards.filter(function (guns) {
+    return guns.type === "gun"
+  }).map((result) => {
+    const card = document.createElement('div');
+    card.classlist = 'card';
+    const description = `<div class="card"> 
+        <h2 class="card-title">${result.title}</h2>
+        <img src="${result.image}" alt="" class="card-img">
+        <p class="card-desc">${result.description}</p>
+      </div>`;
+    container.innerHTML += description;
+  })
+})
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+}})
+buttons.Melees.addEventListener("click", function (filtermelee) {
+  filtermelee.preventDefault();
+        </div>`;
+    container.innerHTML += description;
+  })
+})
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+}})
+const contrabands = [{
+  title: "Force Staff",
+  type: "trinket",
+  image: "",
+  description: "",
+}
+]
+}]
+buttons.Contraband.addEventListener("click", function (filtercontraband) {
+  filtercontraband.preventDefault();
+  container.innerHTML = "";
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else{
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+    }
+  } 
+}) */
